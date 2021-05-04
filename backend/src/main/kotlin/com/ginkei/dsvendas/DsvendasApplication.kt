29b1e68975aa25e1a32cef805e31267c9d7ce977
guina86 <guina86@gmail.com>
2021-05-03 22:55:51 -0300

    Project created

---
+++ backend/src/main/kotlin/com/ginkei/dsvendas/DsvendasApplication.kt
@@ -0,0 +1,11 @@
+package com.ginkei.dsvendas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DsvendasApplication
+
+fun main(args: Array<String>) {
+	runApplication<DsvendasApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/ginkei/dsvendas/DsvendasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ginkei.dsvendas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
