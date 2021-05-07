b5f655a703991180c81465e0163f419e92f727a6
Leandro Guina <guina86@gmail.com>
2021-05-07 14:26:13 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,24 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Sale, SalePage } from "types/sale"
+import { formatLocalDate } from "utils/format"
+import { BASE_URL } from "utils/requests"
+
 const DataTable = () => {
+  const [page, setPage] = useState<SalePage>({
+    first: true,
+    last: true,
+    number: 0,
+    totalElements: 0,
+    totalPages: 0,
+  })
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales?page=1&size=20&sort=date,desc`).then((res) => {
+      setPage(res.data)
+    })
+  }, [])
+
   return (
     <div className="table-responsive">
       <table className="table table-striped table-sm">
@@ -12,13 +32,17 @@ const DataTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
+          {page.content?.map((sale: Sale) => {
+            return (
+              <tr key={sale.id}>
+                <td>{formatLocalDate(sale.date, "dd/MM/yyyy")}</td>
+                <td>{sale.seller.name}</td>
+                <td>{sale.vissited}</td>
+                <td>{sale.deals}</td>
+                <td>{`R$ ${sale.amount.toFixed(2)}`}</td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </div>
